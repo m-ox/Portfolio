72bcdad651ff108e8606b5edd4eef467983b9697
c-masque <maudlin.oxalis@gmail.com>
2021-05-11 10:44:49 -0600

    Class vs Functional Components in React

--- src/components/app.js
+++ src/components/app.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react';
 import moment from "moment";
 
 // best practice to keep importing modules and your own components separated
-import PortfolioContainer from './portfolio-container'
+import PortfolioContainer from './portfolio/portfolio-container'
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
         <h1>Maudlin's Portfolio</h1>
-        <PortfolioContainer/>
         <div> {moment().format('MMMM Do YYYY, h:mm:ss a')} </div>
+        <PortfolioContainer/>
       </div>
     );
   }
---
+++ src/components/portfolio/portfolio-container.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+
+import PortfolioItem from './portfolio-item'
+
+export default class PortfolioContainer extends Component {
+    // state and life cycle hooks need class based component
+    render() { // always need render for class based component
+        return (
+            // JSX looks a lot like html but it isn't~
+            <div>
+                <h2>Portfolio items go here...</h2>
+
+                <PortfolioItem/>
+            </div>
+        )
+    }
+}
+
+// functional and presentation components are the same thing
+
+// dynamic data that can be updated probably would like a class based component, like for forms
+// for just simply rendering content, functional components work just fine
+// portfolio item will just render items as a functional component
+// portfolio-container will be dynamic
+
+// the difference is managing state and having access to life cycle methods
