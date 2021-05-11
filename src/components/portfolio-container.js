994cbcee2299eeeadaf198801c1998c0ab9d5e96
c-masque <maudlin.oxalis@gmail.com>
2021-05-11 10:25:00 -0600

    Introduction to React Components

---
+++ src/components/portfolio-container.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class PortfolioContainer extends Component {
+    render() { // always need render for class based component
+        return (
+            // JSX looks a lot like html but it isn't~
+            <div>
+                <h2>Portfolio items go here...</h2>
+            </div>
+        )
+    }
+}
