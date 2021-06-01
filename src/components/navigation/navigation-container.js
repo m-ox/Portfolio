318e93bb293805877a14ac932d1c300d62fe0d5f
c-masque <maudlin.oxalis@gmail.com>
2021-06-01 14:11:29 -0600

    Navigation component converted to class based component to prepare for maximum hamburger.

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { withRouter } from "react-router"
 import { NavLink } from "react-router-dom"
-import { FaSignOutAlt } from 'react-icons/fa'
 import Axios from 'axios'
 
-const NavigationComponent = (props) => {
-    
-    const dynamicLink = (route, linkText) => {
+import { FaSignOutAlt } from 'react-icons/fa'
+import { AiOutlineMenu } from 'react-icons/ai'
+
+class NavigationComponent extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    dynamicLink(route, linkText) {
         return (
             <div className="nav-link-wrapper">
                 <NavLink to={route} activeClassName="nav-link-active">
@@ -14,14 +19,16 @@ const NavigationComponent = (props) => {
                 </NavLink>
             </div>
         )
+
+        this.handleSignOut = this.handleSignOut.bind(this)
     }
 
-    const handleSignOut =  () => {
+    handleSignOut() {
         Axios.delete('https://api.devcamp.space/logout', {withCredentials: true})
         .then(response => {
             if (response.status === 200) {
-                props.history.push('/')
-                props.handleSuccessfulLogout()
+                this.props.history.push('/')
+                this.props.handleSuccessfulLogout()
             }
             return response.data
         }).catch( error => {
@@ -29,47 +36,57 @@ const NavigationComponent = (props) => {
         })
     }
 
-    return (
-        <div className="nav-wrapper">
-            <div className="left-side">
-
-                <div className="nav-link-wrapper">
-                    <NavLink exact to="/" activeClassName="nav-link-active">
-                        Home 
-                    </NavLink>
+    render() {
+        return (
+            <div className="nav-wrapper">
+    
+                <div className="left-side">
+    
+                    <div className="nav-click" style={{display: 'none'}}>
+                        <a href='#'>
+                            <AiOutlineMenu />
+                        </a>
+                    </div>
+    
+                    <div className="nav-menu">
+                        <div className="nav-link-wrapper">
+                            <NavLink exact to="/" activeClassName="nav-link-active">
+                                Home 
+                            </NavLink>
+                        </div>
+    
+                        <div className="nav-link-wrapper">
+                            <NavLink to="/about-me" activeClassName="nav-link-active">
+                                About Me
+                            </NavLink>
+                        </div>                                                                                                                          
+                        <div className="nav-link-wrapper">
+                            <NavLink to="/contact" activeClassName="nav-link-active">
+                                Contact 
+                            </NavLink>
+                        </div>
+    
+                        <div className="nav-link-wrapper">
+                            <NavLink to="/blog" activeClassName="nav-link-active">
+                                Blog
+                            </NavLink>
+                        </div>
+    
+                        {this.props.loggedInStatus === "LOGGED_IN" ? (this.dynamicLink("/portfolio-manager", "Portfolio Manager")) : null }
+                    </div>
+    
                 </div>
-
-                <div className="nav-link-wrapper">
-                    <NavLink to="/about-me" activeClassName="nav-link-active">
-                        About Me
-                    </NavLink>
+    
+                <div className="right-side">
+                    Maudlin Oxalis
+                    {this.props.loggedInStatus === 'LOGGED_IN' ? <a onClick={this.handleSignOut}>
+                        <FaSignOutAlt className="signout"/>
+                    </a> : null }
                 </div>
-
-                <div className="nav-link-wrapper">
-                    <NavLink to="/contact" activeClassName="nav-link-active">
-                        Contact 
-                    </NavLink>
-                </div>
-
-                <div className="nav-link-wrapper">
-                    <NavLink to="/blog" activeClassName="nav-link-active">
-                        Blog
-                    </NavLink>
-                </div>
-
-                {props.loggedInStatus === "LOGGED_IN" ? (dynamicLink("/portfolio-manager", "Portfolio Manager")) : null }
-
+                
             </div>
-
-            <div className="right-side">
-                Maudlin Oxalis
-                {props.loggedInStatus === 'LOGGED_IN' ? <a onClick={handleSignOut}>
-                    <FaSignOutAlt className="signout"/>
-                </a> : null }
-            </div>
-            
-        </div>
-    )
+        )
+    }
 }
 
 export default withRouter(NavigationComponent)
