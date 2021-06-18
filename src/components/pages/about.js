bb8fdaada8747f50c3a8dfe2c5be05112b427420
c-masque <maudlin.oxalis@gmail.com>
2021-06-18 14:07:26 -0600

    tweaked summary and order of skills

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -7,7 +7,7 @@ let python = 30
 let summary =
     "Montanan web developer, born and raised in the mountains.\n\n\
     I first became interested in coding as a kid when I wanted to make my own video games. I started with learning RPG Maker logic and later in high school I became interested in CSS/JS when I wanted to style my blogs better than my friends.\n\n\
-    During the COVID pandemic, I wanted to make the most out of a lot of down time, and decided to enroll full-time in Bottega University for fullstack development. The curriculum helped fill in a lot of gaps from my informal studies, and now I have a sturdy foundation on a number of technologies. I have found a lot of enjoyment writing Python (it's so clean!) and learning advanced CSS techniques on the side. So far I'd say in this very moment with another month to go into the curriculum, I'm most comfortable writing Python but I have the most fun with React JS."
+    During the COVID pandemic, I wanted to make the most out of a lot of down time, and decided to enroll full-time in Bottega University for fullstack development. The curriculum helped fill in a lot of gaps from my informal studies, and now I have a sturdy foundation on a number of technologies. I have since taken to independent study and am learning the MERN stack."
 
 export default class About extends Component {
     render() {
@@ -29,14 +29,14 @@ export default class About extends Component {
                     <div className="skills-boast">
                         <div className="skill-wrapper">
 
-                            <p>Python</p>
-                            <div className="skill-bar">
-                                <div className="skills py">{python}%</div>
-                            </div>
-
                             <p>Javscript</p>
                             <div className="skill-bar">
                                 <div className="skills js">{js}%</div>
+                            </div>
+
+                            <p>Python</p>
+                            <div className="skill-bar">
+                                <div className="skills py">{python}%</div>
                             </div>
 
                         </div>
