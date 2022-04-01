aa9387c5ddcb92c1f5d7541690c9e03d57685b5c
c-masque <maudlin.oxalis@gmail.com>
2022-04-01 00:12:00 -0600

    updated about section

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
 
-let css = 40
-let js = 30
-let python = 30
+let css = 70
+let js = 70
+let python = 40
 
 let summary =
-    "Montanan web developer, born and raised in the mountains.\n\n\
-    I first became interested in coding as a kid when I wanted to make my own video games. I started with learning RPG Maker logic and later in high school I became interested in CSS/JS when I wanted to style my blogs better than my friends.\n\n\
-    During the COVID pandemic, I wanted to make the most out of a lot of down time, and decided to enroll full-time in Bottega University for fullstack development. The curriculum helped fill in a lot of gaps from my informal studies, and now I have a sturdy foundation on a number of technologies. I have since taken to independent study and am learning the MERN stack."
+    "MERN stack web developer, born and raised in the Rockies.\n\n\
+    I first became interested in coding as a kid when I wanted to make my own video games. I started poking around in RPG Maker and later in high school I became interested in CSS and JS when I wanted to style my blogs better than my friends.\n\n\
+    During the COVID pandemic I felt it was a great time to pivot and make the most of my time, and decided to enroll full-time in Bottega University. The curriculum helped fill in a lot of gaps from my informal dabblings, and now I have a sturdy foundation on a number of technologies. I currently spend my time working on an array of personal projects and building websites for friends and family."
 
 export default class About extends Component {
     render() {
@@ -19,7 +19,7 @@ export default class About extends Component {
                 </div>
 
                 <div className="right-column">
-                    <h3>JavaScript & Python Developer</h3>
+                    <h3>React Developer</h3>
                     <h1 className="about-title">Maudlin Oxalis</h1>
 
                     <div className="boasts">
@@ -29,15 +29,22 @@ export default class About extends Component {
                     <div className="skills-boast">
                         <div className="skill-wrapper">
 
-                            <p>Javscript</p>
+                            <p>JS</p>
                             <div className="skill-bar">
                                 <div className="skills js">{js}%</div>
                             </div>
 
-                            <p>Python</p>
+                            <p>CSS</p>
+                            <div className="skill-bar">
+                                <div className="skills css">{css}%</div>
+                            </div>
+
+                            <p>PY3</p>
                             <div className="skill-bar">
                                 <div className="skills py">{python}%</div>
                             </div>
+
+                            
 
                         </div>
                     </div>
